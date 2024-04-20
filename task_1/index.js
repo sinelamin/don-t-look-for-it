af1d214cd725c02e0a2a4690cdc27fff6e78bf3b
Alexey Mikhaylov <krummi1414@yandex.ru>
2024-04-20 22:52:01 +0300

    add functions decoded and getUniqueIds for task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,7 +1,37 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decoded(encoded, translations) {
+  const exception = ['groupId', 'service', 'formatSize', 'ca'];
+  const translationKeys = Object.keys(translations);
+
+  encoded.forEach(obj => {
+    for (let key in obj) {
+      if (key.endsWith('Id') && !exception.includes(key)) {
+        if (translationKeys.includes(obj[key])) {
+          obj[key] = translations[obj[key]];
+        }
+      }
+    }
+  });
+}
+
+function getUniqueIds(encoded) {
+  const uniqueIds = [];
+
+  encoded.forEach(obj => {
+    for (let key in obj) {
+      if (key.endsWith('Id') && !uniqueIds.includes(key)) {
+        uniqueIds.push(key);
+      }
+    }
+  });
+
+  return uniqueIds;
+}
 
 
-// console.log(decoded)
+decoded(encoded, translations);
+
+console.log("Let's rock");
+console.log(encoded, translations);
+console.log(getUniqueIds(encoded));
