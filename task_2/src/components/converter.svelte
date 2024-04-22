0d2085f879891d41cbf43aea9812a7c85c1e39fd
Alexey Mikhaylov <krummi1414@yandex.ru>
2024-04-22 14:18:17 +0300

    feat: add generation of currency list for inputs

---
+++ task_2/src/components/converter.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+  import { fetchCurrencies } from "../services/currencyApi";
+  import type { CurrencyData } from "../types/currency";
+
+  export let currencyFirst: string = "RUB";
+  export let currencySecond: string = "USD";
+
+  let currencyListFirst: string[] = [];
+  let currencyListSecond: string[] = [];
+
+  async function getCurrencyList(currency: string): Promise<void> {
+    try {
+      const data: CurrencyData = await fetchCurrencies(currency);
+      if (currency === "RUB") {
+        currencyListFirst = Object.keys(data.conversion_rates);
+        console.log(currencyListFirst);
+      } else {
+        currencyListSecond = Object.keys(data.conversion_rates);
+        console.log(currencyListSecond);
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error("Could not load currency rates:", error.message);
+      } else {
+        console.error("Unknown error occurred");
+      }
+    }
+  }
+
+  onMount(() => {
+    getCurrencyList(currencyFirst);
+    getCurrencyList(currencySecond);
+  });
+</script>
+
+<main class="converter">
+  <form class="converter-form">
+    <div class="input__wrapper">
+      <input class="converter-form__input currency--first" type="number" />
+      <select class="currency-list" name="currency">
+        {#each currencyListFirst as currency}
+          <option class="currency-list__item" value={currency}>
+            {currency}
+          </option>
+        {/each}
+      </select>
+    </div>
+
+    <div class="input__wrapper">
+      <input class="converter-form__input currency--second" type="number" />
+      <select class="currency-list" name="currency">
+        {#each currencyListSecond as currency}
+          <option class="currency-list__item" value={currency}>
+            {currency}
+          </option>
+        {/each}
+      </select>
+    </div>
+  </form>
+</main>
+
+<style>
+  .input__wrapper {
+    display: flex;
+    margin-top: 50px;
+    align-items: center;
+    height: 50px;
+  }
+
+  .currency-list {
+    margin-left: 5px;
+    background-color: transparent;
+    border: none;
+  }
+
+  .currency-list__item {
+    background-color: #242424;
+  }
+
+  .converter-form__input {
+    display: block;
+    width: 500px;
+    font-size: 28px;
+    border-radius: 8px;
+  }
+</style>
